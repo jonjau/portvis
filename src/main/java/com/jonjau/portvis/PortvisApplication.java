5a4c3514880f661ae2b517e106d36a81f27776a6
jonjau <jonjau3Q@gmail.com>
2020-01-09 22:25:58 +0700

    initial commit

---
+++ src/main/java/com/jonjau/portvis/PortvisApplication.java
@@ -0,0 +1,13 @@
+package com.jonjau.portvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortvisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonjau/portvis/PortvisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonjau.portvis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
