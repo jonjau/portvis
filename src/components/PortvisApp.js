75cb1a412ea2aa33c7cdc3ae94decf71bfdebffa
jonjau <jonjau3Q@gmail.com>
2020-07-16 21:58:58 +1000

    internal API communication partially working

--- src/components/PortvisApp.js
+++ src/components/PortvisApp.js
@@ -1,34 +1,23 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import "./PortvisApp.css";
+//import "./PortvisApp.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import StockComponent from "./StockComponent";
+import BacktestComponent from "./BacktestComponent";
+import NavigationBar from "./NavigationBar";
 
 class PortvisApp extends Component {
   render() {
     return (
       <Router>
         <>
-          <h1>PORTVIS</h1>
-          <ul>
-            <li>
-              <a class="active" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#news">News</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-          </ul>
+          <NavigationBar />
           <Switch>
             <Route path="/" exact component={StockComponent} />
+            <Route path="/backtest" exact component={BacktestComponent} />
+            {/* <Route render={() => <h1>404: page not found</h1>} /> */}
           </Switch>
         </>
       </Router>
--- src/components/StockComponent.js
+++ src/components/StockComponent.js
@@ -1,15 +1,71 @@
 import React, { Component } from "react";
+import StockService from "../service/StockService";
+import Dygraph from "dygraphs";
 
 class StockComponent extends Component {
-    render() {
-        return (
-            <div>
-                <h3>
-                    HOLA
-                </h3>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stockHistory: [],
+    };
+
+    this.refreshStock = this.refreshStock.bind(this);
+    this.plotHistory = this.plotHistory.bind(this);
+  }
+
+  componentDidMount() {
+    this.refreshStock();
+  }
+
+  refreshStock() {
+    StockService.getStockHistory("lorem ipsum")
+      .then((response) => {
+        //console.log(`received: ${JSON.stringify(response.data, null, 2)}`);
+        this.setState({ stockHistory: response.data });
+      })
+      .catch((error) =>
+        console.log(`error: ${JSON.stringify(error, null, 2)}`)
+      );
+  }
+
+  plotHistory() {
+    const timeSeriesData = this.state.stockHistory.timeSeries;
+
+    const dataArray = Object.keys(timeSeriesData).map((dateTime) => [
+      new Date(dateTime),
+      this.getOHLCAverage(timeSeriesData[dateTime]),
+    ]);
+    //console.log(dataArray);
+    new Dygraph(
+      // this is legacy apparently
+      this.refs.chart,
+      dataArray,
+      {}
+    );
+  }
+
+  getOHLCAverage(prices) {
+    const keys = ["open", "high", "low", "close"];
+    let sum = 0;
+    for (let key of keys) {
+      sum += prices[key];
     }
+    return sum / keys.length;
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>lorem</h3>
+        <textarea
+          defaultValue={JSON.stringify(this.state.stockHistory.timeSeries)}
+        ></textarea>
+        <div ref="chart" />
+        <button onClick={() => this.plotHistory()}>plotHist</button>
+      </div>
+    );
+  }
 }
 
 export default StockComponent;
