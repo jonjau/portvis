af9fae19d7240b769a288c701e69643043ae5640
jonjau <jonjau3Q@gmail.com>
2020-07-17 15:20:16 +1000

    refactoring: about to merge with server code

---
+++ src/service/StockService.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+class CourseDataService {
+    getStockHistory(stockName) {
+        return axios.get(`http://127.0.0.1:8080/query?symbol=a&apikey=1`);
+    }
+}
+
+export default new CourseDataService();
